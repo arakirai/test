3b5dc5d44792114217f3c4e6e5b79786b15834a0
donnma777 <96525816+donnma777@users.noreply.github.com>
2022-09-13 18:18:20 +0900

    2022/09/13　18:18

--- src/components/RegisterData.js
+++ src/components/RegisterData.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import Menu from '..Menu';
-import as React from 'react';
-import Box from '@muimaterialBox';
-import TextField from '@muimaterialTextField';
+// import Menu from '..Menu';
 import axios from axios;
 
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
-const RegisterData = () = {
-    const title = 'ユーザー登録';
-    return(
-        Menu name = { title }
-             h1ユーザー登録h1
-         Menu
+// const RegisterData = () = {
+//     const title = 'ユーザー登録';
+//     return(
+//         Menu name = { title }
+//              h1ユーザー登録h1
+//          Menu
 
 
-    );
-};
+//     );
+// };
 
 const userListData = async() = {
     let res = await axios.get(http49.212.200.1598080apiuserlist);
@@ -24,41 +24,42 @@ const userListData = async() = {
     return res;
 };
 
-const data = { name, age };
-let RegisterData = async() = {
-    let res = await axios.post(http49.212.200.1598080apiuseradd, data);
+const data = { name: "テスト", age: "100" };
+let RegisterData = async () => {
+    let res = await axios.post("http://49.212.200.159:8080/api/user/add", data);
     console.log(res);
     res = res.data;
-    return res;
+    // return res;
 
-    return(
-        Box
-            component = form
-            sx = {{
-            '& .MuiTextField-root' { m 1, width '25ch' },
-        }}
-noValidate
-autoComplete = off
+    return (
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <div>
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="名前"
+                    data name=""
+                />
+                <TextField
+                    disabled
+                    id="outlined-disabled"
+                    label="年齢"
+                    data age=""
+                />
 
-div
-TextField
-required
-id = outlined - required
-label = 名前
-                    data name =
+                    />
+            </div>
 
-    div
-div
-TextField
-required
-id = outlined - required
-label = 年齢
-                    data age =
-
-    div
-
-Box
+        </Box>
     );
-};
 
-export default RegisterData;
+
+
+    export default RegisterData;
